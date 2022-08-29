278362e6674e37fbc412c464d67c1e4c60f13df6
smgroves <sarahmaddoxgroves@gmail.com>
2022-08-29 17:29:44 -0500

    finished app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -16,7 +16,7 @@ def create_app(test_config=None):
         app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        app.config.update(test_config)
 
     # ensure the instance folder exists
     try:
@@ -32,5 +32,17 @@ def create_app(test_config=None):
     from . import db
 
     db.init_app(app)
+
+    from . import auth
+    from . import blog
+
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule("/", endpoint="index")
 
     return app
